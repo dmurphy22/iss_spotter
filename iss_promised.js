362a4b4f2353e616a5ab83b87ae637507c9c437c
Darragh Murphy <dmurphy@servuo.com>
2023-05-12 16:13:09 -0700

    Refactor with promises

---
+++ iss_promised.js
@@ -0,0 +1,28 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}?fields=latitude,longitude,success,message`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  let latitude = JSON.parse(body).latitude;
+  let longitude = JSON.parse(body).longitude;
+  return request(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = {nextISSTimesForMyLocation};
